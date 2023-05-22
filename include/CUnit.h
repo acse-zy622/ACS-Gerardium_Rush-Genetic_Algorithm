edd3ed7288e61f8d0319acdf29302b02c850e5c3
Marijan Beg <m.beg@imperial.ac.uk>
2023-05-22 10:16:26 +0100

    Add acs-gerardium-rush/include/CUnit.h

---
+++ include/CUnit.h
@@ -0,0 +1,23 @@
+/** Header for the unit class
+ * 
+ * 
+ */
+
+#pragma once
+
+class CUnit {
+ public:
+  //index of the unit to which this unit’s concentrate stream is connected 
+  int conc_num;
+  //index of the unit to which this unit’s concentrate stream is connected 
+  int tails_num;
+  //A Boolean that is changed to true if the unit has been seen 
+  bool mark;
+
+  /*
+
+    ...other member functions and variables of CUnit
+
+  */
+};
+
