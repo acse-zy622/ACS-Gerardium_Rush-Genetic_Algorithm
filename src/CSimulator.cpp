4cf68dde2b06598a107f4f4f4a93e404405feb01
ACSE-jd122 <jd122@ic.ac.uk>
2023-05-24 11:52:43 +0100

    Finish CSimulator.cpp

--- src/CSimulator.cpp
+++ src/CSimulator.cpp
@@ -35,20 +35,19 @@ double Evaluate_Circuit(int vector_size, int* circuit_vector, struct Circuit_Par
     CCircuit MyCCircuit((vector_size-1)/2, CCircuit_vector);  // Define the circuit
     MyCCircuit.FillIDs();  // Set IDs of each unit in this circuit
 
-    double Fgi = 10;  // Feed mineral
-    double Fwi = 100;  // Feed waste
-    MyCCircuit.InitialFlowrates(Fgi, Fwi);  // Initialize the flow rate of the circuit
-    MyCCircuit.ReturnFlowrates(flowrates_g, flowrates_g);  // Get initial flow rate of concentrates and tailings
+    double Sg = 10;  // Feed mineral
+    double Sw = 100;  // Feed waste
+    MyCCircuit.SystemFlowrates(Sg, Sw);  // Initialize the flow rate of the circuit
+    MyCCircuit.ReturnFlowrates(flowrates_g, flowrates_w);  // Get initial flow rate of concentrates and tailings
 
-    double max_diff = 0;  // Record maximum change of flow rate
+
+    double max_diff = parameters.tolerance;  // Record maximum change of flow rate
     int cnt = 0;  // Count while loop
-    while(max_diff>parameters.tolerance && cnt < parameters.max_iterations)
+    while(max_diff>=parameters.tolerance && cnt < parameters.max_iterations)
     {
         MyCCircuit.SolveCCircuit(); // Solve mass balance of the circuit
-        swap(flowrates_g, new_flowrates_g);  // Quick assign values from new_flowrates_c to flowrates_c
-        swap(flowrates_w, new_flowrates_w);  // Quick assign values from new_flowrates_t to flowrates_t
         MyCCircuit.ReturnFlowrates(new_flowrates_g, new_flowrates_w);  // Get updated flow rate of concentrates and tailings
-
+        
         for(int n = 0; n < new_flowrates_g.size(); n++)  // Capture maximum change of flow rate
         {
             if(abs(new_flowrates_g[n] - flowrates_g[n]) > max_diff)
@@ -60,6 +59,9 @@ double Evaluate_Circuit(int vector_size, int* circuit_vector, struct Circuit_Par
                 max_diff = abs(new_flowrates_w[n] - flowrates_w[n]);
             }
         }
+        swap(flowrates_g, new_flowrates_g);  // Quick assign values from new_flowrates_c to flowrates_c
+        swap(flowrates_w, new_flowrates_w);  // Quick assign values from new_flowrates_t to flowrates_t
+        cnt++;
     }
     return MyCCircuit.ReturnProfit();  // Calculate and return monetary value of the final concentrate
 }
@@ -71,9 +73,9 @@ double Evaluate_Circuit(int vector_size, int* circuit_vector){
 
 
 // Uncomment the following code to run a simple example
-//int main()
-//{
-//    int vector[11] = {4, 5, 1, 2, 4, 0, 1, 1, 6, 1, 3};
-//    cout << Evaluate_Circuit(11, vector) << endl;
-//    return 0;
-//}
+int main()
+{
+    int vector[11] = {4, 5, 1, 2, 4, 0, 1, 1, 6, 1, 3};
+    cout << Evaluate_Circuit(11, vector) << endl;
+    return 0;
+}
