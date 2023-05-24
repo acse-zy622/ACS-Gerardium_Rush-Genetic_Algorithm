5632fc9cd758afaa4877a9abe003200c10a33abb
acse-zy622 <111791326+acse-zy622@users.noreply.github.com>
2023-05-25 00:57:31 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,44 +4,41 @@
 #include "CUnit.h"
 #include "CSimulator.h"
 #include <chrono>
+
 int main(int argc, char* argv[])
 {
 
     // set things up
-    int vector_size = 11;
+    int vector_size = 13;
 
-    // Set your parameters
+    // set your parameters
     Circuit_Parameters parameters_;
     Algorithm_Parameters parameters;
 
     parameters.population_size = 100;
+    // bounded with mutation_max_step;
     parameters.mutation_rate = 0.05;
-    parameters.crossover_rate = 0.85;
-    parameters.generation_size = 75;
-    parameters.mutation_max_step = 6;
-    parameters.early_stopping_generations = 25;
-    parameters.mutation_type_rate = {0.33,0.67};
-    parameters.crossover_type_rate = {0.33,0.33,0.34};
-
+    parameters.crossover_rate = 0.90;
+    parameters.generation_step = 500;
+    // has somthing to do with the 2*n + 1;
+    parameters.mutation_max_step = vector_size;
+    parameters.early_stopping_generations = 100 + 2*vector_size;
+    parameters.mutation_type_rate = {0.8,0.2};
+    parameters.crossover_type_rate = {0.45,0.45,0.10};
     parameters_.max_iterations = 1000;
     parameters_.tolerance = 0.01;
 
-
-    //Individual I;
-    //I.vector = { 0, 1, 2, 3, 0, 0, 4 };
-    //int num_units = 3; // Initialize with appropriate value
-    //Circuit circuit(num_units);
-
-    //if (circuit.Check_Validity(I.vector.data())) {
-    //    std::cout << "The circuit is valid.\n";
-    //}
-    //else {
-    //    std::cout << "The circuit is not valid.\n";
-    //}
-    //23250041006
-    // run your code
+    // unit_test and 23250041006 is the unit case
+    auto total_start_ = std::chrono::high_resolution_clock::now();
+    double result_ = optimize(vector_size,Evaluate_circuit,parameters);
+    auto total_end_ = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> total_elapsed_ = total_end_ - total_start_;
+    std::cout << result_ << std::endl;
+    std::cout << "Total running time: " << total_elapsed_.count() << " seconds" << std::endl;
+    std::cout << "                      " << std::endl;
+    // run your real code here
     auto total_start = std::chrono::high_resolution_clock::now();
-    double result = optimize(vector_size,Evaluate_Circuit,parameters);
+    double result = optimize(vector_size, Evaluate_Circuit, parameters);
     auto total_end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> total_elapsed = total_end - total_start;
     std::cout << result << std::endl;
