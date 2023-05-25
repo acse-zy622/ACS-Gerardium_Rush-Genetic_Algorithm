8161adffd3144a816036cd486c814c31b67daeb4
acse-ra922 <110524155+acse-ra922@users.noreply.github.com>
2023-05-25 10:57:51 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 
 int main(int argc, char* argv[])
 {
+    int end = 0;
+    while(end!=-1)
+    {
     std::cout << "Flourite Germaninum Rush" << std::endl;
     // set things up
     int vector_size;
@@ -24,12 +27,47 @@ int main(int argc, char* argv[])
     Circuit_Parameters parameters_;
     Algorithm_Parameters parameters;
 
-    parameters.population_size = 100;
-    parameters.mutation_rate = 0.05;
-    parameters.crossover_rate = 0.85;
-    parameters.generation_size = 500;
+    std::cout << "Please enter the population size" << std::endl;
+    std::cout << "To use the default value(100), please enter 0" << std::endl;
+    std::cin >> parameters.population_size;
+    if (parameters.population_size == 0)
+    {
+        parameters.population_size = 100;
+    }
+
+    std::cout << "Please enter the mutation rate" << std::endl;
+    std::cout << "To use the default value(0.05), please enter 0" << std::endl;
+    std::cin >> parameters.mutation_rate;
+    if (parameters.mutation_rate == 0)
+    {
+        parameters.mutation_rate = 0.05;
+    }
+
+    std::cout << "Please enter the crossover rate" << std::endl;
+    std::cout << "To use the default value(0.85), please enter 0" << std::endl;
+    std::cin >> parameters.mutation_rate;
+    if (parameters.crossover_rate == 0)
+    {
+        parameters.crossover_rate = 0.85;
+    }
+
+    std::cout << "Please enter the generation size" << std::endl;
+    std::cout << "To use the default value(500), please enter 0" << std::endl;
+    std::cin >> parameters.generation_size;
+    if (parameters.generation_size == 0)
+    {
+        parameters.generation_size = 500;
+    }
+
     parameters.mutation_max_step = 5;
-    parameters.early_stopping_generations = 500;
+    
+    std::cout << "Please enter the early stopping generations" << std::endl;
+    std::cout << "To use the default value(500), please enter 0" << std::endl;
+    std::cin >> parameters.early_stopping_generations;
+    if (parameters.early_stopping_generations == 0)
+    {
+        parameters.early_stopping_generations = 500;
+    }
     parameters.mutation_type_rate = { 0.33,0.33,0.34 };
     parameters.crossover_type_rate = { 0.33,0.33,0.34 };
 
@@ -41,5 +79,10 @@ int main(int argc, char* argv[])
     // Generate final output
     std::cout << result << std::endl;
 
+    std::cout<<"To continue using the program enter any number"<<std::endl;
+    std::cout<<"To exit, please enter -1"<<std::endl;
+    std::cin>>end;
+    }
     return 0;
+
 }
