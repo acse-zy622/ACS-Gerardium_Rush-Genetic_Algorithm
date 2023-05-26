a2fb43847d833b5c87bd2fc8f94334595230b898
acse-ra922 <110524155+acse-ra922@users.noreply.github.com>
2023-05-26 09:25:55 +0100

    Update HyperPSearch.h

--- include/HyperPSearch.h
+++ include/HyperPSearch.h
@@ -1,7 +1,12 @@
 /**
 *@brief header file to perform hyperparamter search
 **/
+#ifndef HyperPSearch_H
+#define HyperPSearch_H
 #include "Genetic_Algorithm.h"
 
 Algorithm_Parameters generateRandomParameters(int vector_size);
 void randomSearch(int iterCount, int* best_vector, int vector_size);
+
+
+#endif
